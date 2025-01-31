6f98001afc4fb252df58c239200253382307651d
PhemC <theoix26@gmail.com>
2025-01-31 12:19:36 +0100

    add weapons

--- Code/Item.hpp
+++ Code/Item.hpp
@@ -16,4 +16,45 @@ protected:
 public:
 	Money(int _amount = 1);
 	void interact(Player& player) override;
+
+
+
+//Weeeeeeeeeeeeeeeeeeeeeeeapons
+class Sword : public Entity, public Item {
+protected:
+	int amount;
+public:
+	bool swordacquired = false;
+
+
+	Sword(int _amount = 1);
+	void interact(Player& player) override;
+	void swordattack();
+
+};
+
+class Axe : public Entity, public Item {
+protected:
+	int amount;
+public:
+	bool axeacquired = false;
+
+
+	Axe(int _amount = 1);
+	void interact(Player& player) override;
+	void axeattack();
+
+};
+
+class Dagger : public Entity, public Item {
+protected:
+	int amount;
+public:
+	bool daggeracquired = false;
+
+
+	Dagger(int _amount = 1);
+	void interact(Player& player) override;
+	void daggerattack();
+
 };
